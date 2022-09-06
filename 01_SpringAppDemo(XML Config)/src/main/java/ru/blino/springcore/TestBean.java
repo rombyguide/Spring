529bc7683ba45d1d944edce508c1531e2eea47e6
Roman Blinov <romanblinov1613@gmail.com>
2022-09-06 22:55:39 +0300

    add first part module Spring core

---
+++ 01_SpringAppDemo(XML Config)/src/main/java/ru/blino/springcore/TestBean.java
@@ -0,0 +1,20 @@
+package ru.blino.springcore;
+
+/**
+ * @author Roman Blinov
+ */
+public class TestBean {
+    private String name;
+
+    public TestBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ 01_SpringAppDemo(XML Config)/src/main/java/ru/blino/springcore/TestSpring.java
@@ -0,0 +1,20 @@
+package ru.blino.springcore;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author Roman Blinov
+ */
+public class TestSpring {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+
+        System.out.println(testBean.getName());
+
+        context.close();
+    }
+}
